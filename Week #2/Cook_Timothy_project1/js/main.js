9401275af1826b7769a3f1579ed10bfc5ce0cbdc
Tim Cook <redeemingdesign@gmail.com>
2012-08-09 19:25:34 -0500

    Still not done…
    I WILL get this junk done tomorrow!

--- Week #2/Cook_Timothy_project1/js/main.js
+++ Week #2/Cook_Timothy_project1/js/main.js
@@ -3,21 +3,21 @@
 	Project 2
 	VFW Full Sail
 */
-
+//wait until the DOM is ready
 window.addEventListener("DOMContentLoaded", function(){
-	
-	
-	function $(x) {
+	//alert(localStorage.value(0));
+	//getElementById Function
+	function $(x){
 		var theElement = document.getElementById(x);
 		return theElement;
 	}
-	
-	function pickColor() {
+	//Create select field element and populate with options.
+	function pickColor(){
 		var formTag = document.getElementsByTagName("form"),
 			selectLi = $('select'),
 			makeSelect = document.createElement('select');
 			makeSelect.setAttribute("id", "colors");
-		for (var i=0, j=selectColor.length; i<j; i++) {
+		for (var i=0, j=selectColor.length; i<j; i++){
 			var makeOption = document.createElement('option');
 			var optText = selectColor[i];
 			makeOption.setAttribute("value", optText);
@@ -27,17 +27,56 @@ window.addEventListener("DOMContentLoaded", function(){
 		selectLi.appendChild(makeSelect);
 	}
 	
-	var selectColor = ["--Choose A Color--", "Black", "White", "Silver", "Red", "Blue", "Yellow", "Green"];
+	// Find value of selected radio button.
+/*	function getSelectedRadio(){
+		var radios = document.forms[0].condition;
+		for (var i=0, i<radios.length; i++){
+			if(radios[i].checked){
+				conditionValue = radios[i].value;
+			}
+		}
+	}
+	
+	function getCheckboxValue(){
+		if($('display').checked){
+			displayValue
+		}
+	}
+	*/
+	function storeData() {
+		var id				= Math.floor(Math.random()*1000000);
+		//Gather all form field values and store in an object.
+		//Object properties contain array with form label and input values.
+		getSelectedRadio();
+	//	getCheckboxValue();
+		var car				= {};
+			car.make 		= ["Make:", $('make').value];
+			car.model		= ["Model:", $('model').value];
+			car.year		= ["Year:", $('year').value];
+			car.doors		= ["Number of doors:", $('doors').value];
+			car.color 		= ["Color:", $('color').value];
+			//car.display		= ["What makes it stand out?", displayValue];
+			//car.condition	= ["What's the condition like?", conditionValue];
+			car.discribe	= ["Discribe the car in your own words.", $('discribe').value];
+		// Save data to local storage: Use Strinify to convert our object to a sting.
+		localStorage.setItem(id, JSON.stringify(car));
+		alert("Car Tagged!");
+	}
+	
+	//Variable defaults
+	var selectColor = ["--Choose A Color--", "Black", "White", "Silver", "Red", "Blue", "Yellow", "Green"],
+		conditionValue
+	;
 	pickColor();
 	
+	//Set link and submit click events
 	/*
 	var displayLink = $('displayLink');
 	displayLink.addEventListener("click", getData);
 	var clearLink = $('clear');
-	clearLink.addEventListener("click", clearLocal);
+	clearLink.addEventListener("click", clearLocal); */
 	var save = $('submit');
 	save.addEventListener("click", storeData);
-	*/
 });
 
 
