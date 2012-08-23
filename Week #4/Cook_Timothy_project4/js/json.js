f22a833cf96883b650331e9fcb43e47060ef64df
Tim Cook <redeemingdesign@gmail.com>
2012-08-23 12:39:16 -0500

    mostly done
    Just adding the search functionality

--- Week #4/Cook_Timothy_project4/js/json.js
+++ Week #4/Cook_Timothy_project4/js/json.js
@@ -5,24 +5,24 @@
 
 var json = {
 	"car1": {
-		"make": ["Make: ", "Ford"],
-		"model": ["Model: ", "Focus"],
-		"year": ["Year: ", "2009"],
-		"doors": ["Number of doors: ", "4"],
-		"colors": ["Color: ", "Red"],
-		"display": ["What makes it stand out? ", "Sound"],
-		"condition": ["Condition: ", "Not"],
-		"describe": ["Describe the car in your own words. ", "I like it much."]
+		"make": 		["Make: ", "Porsche"],
+		"model": 		["Model: ", "911"],
+		"year": 		["Year: ", "2009"],
+		"doors": 		["Number of doors: ", "2"],
+		"colors": 		["Color: ", "Black"],
+		"display": 		["What makes it stand out? ", ["Sound", "Fast", "Rims"]],
+		"condition": 	["Condition: ", "Amazing"],
+		"describe": 	["Describe the car in your own words. ", "I like it much."]
 	},
 	"car2": {
-		"make": ["Make: ", "Nissan"],
-		"model": ["Model: ", "Maxima"],
-		"year": ["Year: ", "2004"],
-		"doors": ["Number of doors: ", "4"],
-		"colors": ["Color: ", "Black"],
-		"display": ["What makes it stand out? ", "Fast"],
-		"condition": ["Condition: ", "Amazing"],
-		"describe": ["Describe the car in your own words. ", "I like it muchly."]
+		"make": 		["Make: ", "Ferrari"],
+		"model": 		["Model: ", "458"],
+		"year": 		["Year: ", "2012"],
+		"doors": 		["Number of doors: ", "2"],
+		"colors": 		["Color: ", "Red"],
+		"display": 		["What makes it stand out? ", ["Fast", "Rims"]],
+		"condition": 	["Condition: ", "Amazing"],
+		"describe": 	["Describe the car in your own words. ", "I like it muchly."]
 	}
 
 
